ecc330f4cfdbfde5ac7874c8db6ce7d00ac676dd
Joshua Volz <jdvolz@gmail.com>
2011-08-14 18:44:36 -0700

    fixed issue to standardize result with David's algorithm results

--- go.rb
+++ go.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative ENV['ALGORITHM'] || 'kata'
 result = Anagram.two_word_anagrams_of("documenting")
-
+result = result.collect{|x| x.split(' ').sort{|a,b| a.length <=> b.length}.join(' ')}
 correct = (result.sort == ["dingo centum", "doing centum", "dung centimo", "dunting come", "genomic dunt", "guid contemn", "gunmen dicot", "induct genom", "induct gnome", "mendigo cunt", "meno ducting", "mignon educt", "mount ceding", "munting code", "munting coed", "munting deco", "mute condign", "muted coning", "muting coden", "muting coned", "muton ceding", "nome ducting", "nonce midgut", "notum ceding", "omen ducting", "omening duct", "tonemic dung", "tonged cumin", "tonged mucin", "tongmen duci", "tuned coming", "tuned gnomic", "tung demonic", "ungot minced", "unmet coding"].collect{|x| x.split(' ').sort{|a,b| a.length <=> b.length}.join(' ')}.sort)
 
 if !correct
