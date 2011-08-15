13e4d3156a6e5c0b2a85e98ed9ca96b6b82c9a0b
Paul Grayson <pdg@alum.mit.edu>
2011-08-15 01:24:54 -0700

    added paul's algorithm

---
+++ paul.rb
@@ -0,0 +1,45 @@
+require 'matrix'
+
+module Anagram
+  @@zeros = 26.times.collect {0}
+  
+  def self.to_vector(word)
+    array = @@zeros.dup
+    word.each_byte { |x| array[x-65] += 1}
+    Vector[*array]
+  end
+
+  def self.two_word_anagrams_of(string)
+    s = string.upcase
+    target = to_vector(s)
+    letters_in_target = {}
+    s.each_char { |c| letters_in_target[c] = true }
+
+    vec_hash = {}
+    ret = []
+
+    open('dictionary_full.txt','rb').each do |word|
+      word.chomp!
+
+      bad = false
+      word.each_char do |letter|
+        if !letters_in_target[letter]
+          bad = true
+          break
+        end
+      end
+      next if bad
+
+      vec = to_vector(word)
+      diff = target - vec
+      if others = vec_hash[diff]
+        others.each do |other_word|
+          ret << "#{other_word} #{word}".downcase
+        end
+      end
+      (vec_hash[vec] ||= []) << word
+    end
+
+    ret
+  end
+end
